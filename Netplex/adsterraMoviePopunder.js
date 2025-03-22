5285b58f2a329b3d56579093ddc926733b9e5058
zineflix <zineflix0@gmail.com>
2025-03-23 07:50:00 +0800

    Update adsterraMoviePopunder.js

--- Netplex/adsterraMoviePopunder.js
+++ Netplex/adsterraMoviePopunder.js
@@ -81,7 +81,7 @@ function openPopupContainer() {
     popup.appendChild(preloadedAdIframe);
     document.body.appendChild(popup);
 
-    let timer = 10; // Reduced countdown to 10 seconds for faster skip
+    let timer = 15; // Reduced countdown to 15 seconds for faster skip
     countdown.innerText = `Loading ad... Please wait ${timer} seconds to Skip...`;
     const interval = setInterval(() => {
         timer--;
