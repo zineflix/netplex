b6c437b71eb76e89219ad3bdbf2ca8f1c9f47961
zineflix <zineflix0@gmail.com>
2025-03-25 23:27:30 +0800

    Update movie.js

--- Netplex/1234/movie.js
+++ Netplex/1234/movie.js
@@ -245,15 +245,14 @@ function openSearchPage() {
 const MOVIE_ENDPOINTS = [
     { url: 'https://vidsrc.cc/v2/embed/movie/', name: 'Server 1' },
     { url: 'https://player.videasy.net/movie/', name: 'Server 2' },
-    { url: 'https://111movies.com/movie/', name: 'Server 3' },
-    { url: 'https://embed.rgshows.me/api/1/movie/?id=', name: 'Server 4' },
-    { url: 'https://embed.rgshows.me/api/3/movie/?id=', name: 'Server 5' },
-    { url: 'https://rivestream.org/embed?type=movie&id=', name: 'Server 6' },
-    { url: 'https://vidsrc.rip/embed/movie/', name: 'Server 7' },
-    { url: 'https://vidsrc.cc/v3/embed/movie/', name: 'Server 8' },
-    { url: 'https://vidfast.pro/movie/', name: 'Server 9' },
-    { url: 'https://vidsrc.su/embed/movie/', name: 'Server 10' },
-    { url: 'https://111movies.com/movie/', name: 'Server 11' },
+    { url: 'https://vidfast.pro/movie/', name: 'Server 3' },
+    { url: 'https://vidsrc.su/embed/movie/', name: 'Server 4' },
+    { url: 'https://111movies.com/movie/', name: 'Server 5' },
+    { url: 'https://embed.rgshows.me/api/1/movie/?id=', name: 'Server 6' },
+    { url: 'https://embed.rgshows.me/api/3/movie/?id=', name: 'Server 7' },
+    { url: 'https://rivestream.org/embed?type=movie&id=', name: 'Server 8' },
+    { url: 'https://vidsrc.rip/embed/movie/', name: 'Server 9' },
+    { url: 'https://vidsrc.cc/v3/embed/movie/', name: 'Server 10' },
 ];
 
 // Get the movie ID from the URL query string
