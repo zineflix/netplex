dcdeb687aa0559e7aabce8bada1a1c55b9e47b6d
zineflix <zineflix0@gmail.com>
2025-04-01 10:11:52 +0800

    Update movie-details.js

--- Netplex/1234/movie-details.js
+++ Netplex/1234/movie-details.js
@@ -537,11 +537,4 @@ document.addEventListener("DOMContentLoaded", async function () {
     }
 });
 
-// SERVICE WORKER FOR OG
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/service-worker.js').then((registration) => {
-    console.log('Service Worker registered with scope:', registration.scope);
-  }).catch((error) => {
-    console.log('Service Worker registration failed:', error);
-  });
-}
+
