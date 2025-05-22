d6ab067199a75fb871b2e8e2761e2c09d73b3f86
zineflix <zineflix0@gmail.com>
2025-05-23 06:08:36 +0800

    Update ads.js

--- Netplex/ads.js
+++ Netplex/ads.js
@@ -1,4 +1,3 @@
-// ads.js
 const adContainer = document.querySelector('.ads-row');
 
 const adSources = [
@@ -7,17 +6,28 @@ const adSources = [
   "https://beddingfetched.com/xfeh17rkn?key=7ede7876efcc5a81490997f5911d84d5"
 ];
 
-adSources.forEach(src => {
-  const wrapper = document.createElement("div");
-  wrapper.className = "responsive-iframe-container";
+function loadAds() {
+  // Clear previous ads
+  adContainer.innerHTML = '';
 
-  const iframe = document.createElement("iframe");
-  iframe.src = src;
-  iframe.width = "100%";
-  iframe.height = "100%";
-  iframe.style.border = "none";
-  iframe.loading = "lazy";
+  adSources.forEach(src => {
+    const wrapper = document.createElement("div");
+    wrapper.className = "responsive-iframe-container";
 
-  wrapper.appendChild(iframe);
-  adContainer.appendChild(wrapper);
-});
+    const iframe = document.createElement("iframe");
+    iframe.src = src;
+    iframe.width = "100%";
+    iframe.height = "100%";
+    iframe.style.border = "none";
+    iframe.loading = "lazy";
+
+    wrapper.appendChild(iframe);
+    adContainer.appendChild(wrapper);
+  });
+}
+
+// Initial load
+loadAds();
+
+// Reload ads every 30 minutes (30 * 60 * 1000 ms)
+setInterval(loadAds, 30 * 60 * 1000);
