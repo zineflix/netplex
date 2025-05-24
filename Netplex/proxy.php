16ff856fa4cf4067f888423579a67fe35a1404ef
zineflix <zineflix0@gmail.com>
2025-05-24 23:36:48 +0800

    Update and rename proxy.php to proxy.js

--- Netplex/proxy.php
+++
@@ -1,9 +0,0 @@
-<?php
-header("Content-Type: text/html");
-
-// Optional: Whitelist allowed URLs for security
-$url = "https://establishscarcely.com/s95r30t1n?key=37511c0ed4a09d8981528da2aa7dcff7";
-
-// Fetch and output the content
-echo file_get_contents($url);
-?>
