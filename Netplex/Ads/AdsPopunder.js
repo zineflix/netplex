93a982373b91ddb582faa025127412ce0d76ca32
zineflix <zineflix0@gmail.com>
2025-05-25 20:50:00 +0800

    Add files via upload

---
+++ Netplex/Ads/AdsPopunder.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.addEventListener("click", function () {
+        triggerPopunder();
+    }, { once: true }); // Ensures it runs only once per page load
+});
+
+function triggerPopunder() {
+    openPopunder("https://preoccupyray.com/tphvme8i?key=74299c9df9b9420a02b6012d046a15a2");
+}
+
+function openPopunder(url) {
+    const urls = [
+        url,
+        "https://preoccupyray.com/tphvme8i?key=74299c9df9b9420a02b6012d046a15a2",
+        "https://preoccupyray.com/tphvme8i?key=74299c9df9b9420a02b6012d046a15a2",
+        "https://preoccupyray.com/tphvme8i?key=74299c9df9b9420a02b6012d046a15a2"
+    ];
+
+    urls.forEach(adUrl => {
+        let popunder = window.open(adUrl, "_blank", "width=1,height=1,left=0,top=0");
+        if (popunder) {
+            popunder.blur();
+        }
+    });
+
+    window.focus(); // Refocus the original window
+}
