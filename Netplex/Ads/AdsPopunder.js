66141ec6e8c9f3f78f99343d2cfe8e24de043bc1
zineflix <zineflix0@gmail.com>
2025-06-02 18:21:46 +0800

    Update AdsPopunder.js

--- Netplex/Ads/AdsPopunder.js
+++ Netplex/Ads/AdsPopunder.js
@@ -14,7 +14,7 @@ function openPopunder(url) {
         "https://preoccupyray.com/wi0ysdhc?key=255bd01e810de84ae6fd6404001fb5e3",
         "https://preoccupyray.com/atnj41y6?key=99e4a912dedac18eecb9005ee8e985b5",
         "https://preoccupyray.com/djmi4vcmx?key=e9e5a858ea141d418fa297b2a3e32eaf",
-        "https://preoccupyray.com/tphvme8i?key=74299c9df9b9420a02b6012d046a15a2",
+        "https://10183.xml.4armn.com/direct-link?pubid=975170&siteid=[SITE_ID]",
         "https://preoccupyray.com/wi0ysdhc?key=255bd01e810de84ae6fd6404001fb5e3",
         "https://preoccupyray.com/atnj41y6?key=99e4a912dedac18eecb9005ee8e985b5",
         "https://preoccupyray.com/djmi4vcmx?key=e9e5a858ea141d418fa297b2a3e32eaf"
