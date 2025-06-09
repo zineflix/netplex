73a2b28a17d0eac51a9e9f2a4dabaaf13bd8de1f
zineflix <zineflix0@gmail.com>
2025-06-09 17:00:00 +0800

    Update AdsPopunder.js

--- Netplex/Ads/AdsPopunder.js
+++ Netplex/Ads/AdsPopunder.js
@@ -11,8 +11,12 @@ function triggerPopunder() {
 function openPopunder(url) {
     const urls = [
         url,
-        "https://otieu.com/4/7335666",
+        "https://preoccupyray.com/tphvme8i?key=74299c9df9b9420a02b6012d046a15a2",
         "https://preoccupyray.com/atnj41y6?key=99e4a912dedac18eecb9005ee8e985b5",
+        "https://preoccupyray.com/djmi4vcmx?key=e9e5a858ea141d418fa297b2a3e32eaf",
+        "https://preoccupyray.com/b833j74w?key=17bad4f9f46748fe4f13b92fed12a99d",
+        "https://preoccupyray.com/y42xrxdr?key=478b92749e54c9f3609811242c2f4121",
+        "https://preoccupyray.com/kgsz4b6ybr?key=8078fe7c08e013e9278d64a634df9a56",
         "https://pulseadnetwork.com/jump/next.php?r=10029718"
     ];
 
