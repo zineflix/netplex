f6bc8ab13e3a98bee39ec295eb6002da2237d3f5
zineflix <zineflix0@gmail.com>
2025-07-02 09:51:09 +0800

    Create galaksion.js

---
+++ Netplex/Ads/galaksion.js
@@ -0,0 +1,7 @@
+// Call your script dynamically
+const scriptElement = document.createElement('script');
+scriptElement.src = "//xn.ariledreinfer.com/rQx3ZYA6wM2gva/122481";
+scriptElement.async = true;
+scriptElement.setAttribute('data-cfasync', 'false');
+
+document.body.appendChild(scriptElement);
