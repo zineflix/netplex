28114078c7da1653d2689b307d9f0cc9bacbcd61
zineflix <zineflix0@gmail.com>
2025-07-25 20:24:43 +0800

    Update AdsPopunder.js

--- Netplex/Ads/AdsPopunder.js
+++ Netplex/Ads/AdsPopunder.js
@@ -1,4 +1,4 @@
-(const popunderUrl = "https://boredomcuff.com/k3f2mnd5nf?key=5b3642b3df8de5a4268e2002a5af413d"; // Replace with your actual direct link
+(const popunderUrl = "DL here"; // Replace with your actual direct link
 
 function openPopunder() {
   const features = "width=1,height=1,left=9999,top=9999";
