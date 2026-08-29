6e9dc3f2a2bd9d9ba8a568acb659c40d675aeb61
zineflix <zineflix0@gmail.com>
2026-08-29 20:30:08 +0800

    Update home.js

--- Netplex/1234/home.js
+++ Netplex/1234/home.js
@@ -1,7 +1,7 @@
 // ===================================
 // 1. TMDB CONFIG
 // ===================================
-const apiKey = "a1PbWWqgKDBDorh525uecKaGZD21FGSoCeR";
+const apiKey = "a1e72fd93ed59f56e6332813b9f8dcae";
 const baseURL = "https://api.themoviedb.org/3";
 const imgURL = "https://image.tmdb.org/t/p/w500";
 
